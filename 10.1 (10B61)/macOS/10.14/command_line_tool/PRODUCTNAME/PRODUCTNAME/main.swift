1d14f703249b7e8070311221f636d0ad6625e0b2
Jan Nash <jnash@jnash.de>
2019-01-18 21:42:00 +0100

    Add Xcode 10.1 (10B61), macOS 10.14, command line tool

---
+++ 10.1 (10B61)/macOS/10.14/command_line_tool/PRODUCTNAME/PRODUCTNAME/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PRODUCTNAME
+//
+//  Created by Jan Nash on 18.01.19.
+//  Copyright © 2019 ORGANIZATIONNAME. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
