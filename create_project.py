309289cd1cf8c6c30bb8a38c8a77486f3ef37faf
Jan Nash <jnash@jnash.de>
2019-01-19 02:18:41 +0100

    Start implementing Template class

--- create_project.py
+++ create_project.py
@@ -6,13 +6,36 @@ __license__ = "BSD-3-Clause"
 
 VERSION = "%s v" + __version__
 
+import os
+import logging
+
+
+
+class Template():
+    def __init__(self, xcode_version='10.1 (10B61)', platform='macOS', os_version='10.14', template_type='command line tool'):
+        self.xcode_version = xcode_version
+        self.platform = platform
+        self.os_version = os_version
+        self.template_type = template_type
+        logging.debug('Created Template ')
+
+    def __repr__(self):
+        return 'Template({}, {}, {}, {})'.format(self.xcode_version, self.platform, self.os_version, self.template_type)
+
+    @property
+    def path_relative_to_template_folder(self):
+        return 
+
+
+def download_template(template: Template):
+    pass
+
 
 def parse_args():
     """
     Parses command-line arguments and returns the parsed namespace-object.
     """
     import argparse
-    import os
 
     parser = argparse.ArgumentParser(version=VERSION)
 
@@ -25,8 +48,6 @@ def parse_args():
 
 
 def main(cmd_args):
-    import logging
-    
     if cmd_args.debug:
         logging.basicConfig(level=logging.DEBUG)
     else:
