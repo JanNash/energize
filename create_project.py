28cc0c6607e5e52cbf72b4c855ccb69aca756f59
Jan Nash <jnash@jnash.de>
2019-01-19 02:00:26 +0100

    Create parse_args and main functions

--- create_project.py
+++ create_project.py
@@ -7,9 +7,32 @@ __license__ = "BSD-3-Clause"
 VERSION = "%s v" + __version__
 
 
-def main():
-    pass
+def parse_args():
+    """
+    Parses command-line arguments and returns the parsed namespace-object.
+    """
+    import argparse
+    import os
+
+    parser = argparse.ArgumentParser(version=VERSION)
+
+    parser.add_argument(
+        '-d', '--debug',
+        action='store_true', default=False, dest='debug',
+        help='Set logging level to DEBUG. Beware, it\'s gonna be verbose :).')
+
+    return parser.parse_args()
+
+
+def main(cmd_args):
+    import logging
+    
+    if cmd_args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
 
 
 if __name__ == '__main__':
-    main()
+    cl_args = parse_args()
+    main(cl_args)
