ca81d301ade5d379f73a2be6270a959720428d97
Jan Nash <jnash@jnash.de>
2019-01-19 01:57:43 +0100

    Create create_project script

---
+++ create_project.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+__author__ = 'Jan Nash'
+__version__ = "0.1"
+__license__ = "BSD-3-Clause"
+
+VERSION = "%s v" + __version__
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
